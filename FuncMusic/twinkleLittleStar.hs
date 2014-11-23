b49bbbe983ff71f0a5d74009569567c5eba722fb
Alice Wonderland <alicedat12@student.lth.se>
2014-11-23 13:20:35 +0100

    added twinkle twinkle little star

---
+++ FuncMusic/twinkleLittleStar.hs
@@ -0,0 +1,22 @@
+module TwinkleTwinkleLittleStar where
+import Haskore
+
+-- note updaters for mappings
+fd d n = n d v
+vol n = n v
+v = [Volume 80]
+lmap f l = line (map f l)
+
+-- Main voice
+p1 = lmap vol [g 4 qn, g 4 qn, d 4 qn, d 4 qn, e 4 qn, e 4 qn, d 4 hn]
+
+p2 = lmap vol [c 4 qn, c 4 qn, b 4 qn, b 4 qn, a 4 qn, a 4 qn, g 4 hn]
+
+p3 = lmap vol [d 4 qn, d 4 qn, c 4 qn, c 4 qn, b 4 qn, b 4 qn, a 4 hn]
+
+mainVoice = p1 :+: p2 :+: p3 :+: p3 :+: p1 :+: p2
+
+
+
+-- Finish
+twinkleLittleStar = Instr "piano" (Tempo 3 (Phrase [Dyn SF] mainVoice))
